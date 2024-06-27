18654cacfa3e5eea8e3304c1d720b936587af119
TheDigitalNinja <dsw.ninjaboy@gmail.com>
2024-06-26 21:50:22 -0600

    fix vertical alignment on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Header title="The Digital Ninja"/>
-      <main className="flex min-h-screen items-center justify-center">
+      <main className="flex-grow flex items-center justify-center">
         <h1 className="text-4xl font-bold">The Digital Ninja</h1>
       </main>
-    </>
+    </div>
   );
 }
