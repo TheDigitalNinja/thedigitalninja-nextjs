552f008322a23d62a7257d997b6ba9fc90f08325
TheDigitalNinja <dsw.ninjaboy@gmail.com>
2024-06-30 14:55:51 -0600

    redirect www to non-www

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    redirects: async () => {
+      return [
+        {
+          source: '/:path*',
+          has: [{ type: 'header', key: 'host', value: 'www.thedigital.ninja' }],
+          destination: 'https://TheDigital.Ninja/:path*',
+          permanent: true,
+        },
+      ];
+    },
+  }
 
 export default nextConfig;
