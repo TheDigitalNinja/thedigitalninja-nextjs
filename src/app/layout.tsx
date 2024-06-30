941b166a87acfd2b0df7cd4424bae0bfb438836c
TheDigitalNinja <dsw.ninjaboy@gmail.com>
2024-06-29 22:50:04 -0600

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Footer from "../components/Footer";
+import Script from 'next/script';
 import "../styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,6 +19,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script 
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+                {`
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){window.dataLayer.push(arguments);}
+                  gtag('js', new Date());
+
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>
         {children}
         <Footer />
