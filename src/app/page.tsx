c34e9ab8c38bf88974a60035296af0065f111f50
TheDigitalNinja <dsw.ninjaboy@gmail.com>
2024-07-07 15:28:45 -0600

    add metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,15 +10,30 @@
 
 import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
-import Head from 'next/head';
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'The Digital Ninja - Russell Perkins',
+  description: 'Explore tech insights, software architecture, and AI with Russell Perkins, a seasoned Solutions Architect and IT consultant.',
+  openGraph: {
+    title: 'The Digital Ninja - Russell Perkins',
+    description: 'Dive into tech insights, software architecture, and AI with Russell Perkins. Discover innovative solutions and industry expertise.',
+    type: 'website',
+    url: 'https://TheDigital.Ninja',
+    images: [
+      {
+        url: 'https://res.cloudinary.com/TheDigitalNinja/image/upload/logo-white-bg_uk6pkk.jpg',
+        width: 1200,
+        height: 1200,
+        alt: 'The Digital Ninja Logo',
+      },
+    ],
+  },
+}
 
 export default function Home() {
   return (
     <>
-      <Head> 
-        <link rel="canonical" href="https://TheDigital.Ninja" />
-      </Head>
-
       <div className="flex flex-col min-h-screen">
         <Header title="The Digital Ninja"/>
         <div className="flex flex-1">
