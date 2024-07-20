a59093f35cf7bb44ce6fd7d75f7f86d32b2e355c
TheDigitalNinja <dsw.ninjaboy@gmail.com>
2024-07-20 14:00:27 -0600

    dont auto generate robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://TheDigital.Ninja',
-    generateRobotsTxt: true,
+    generateRobotsTxt: false,
   }
