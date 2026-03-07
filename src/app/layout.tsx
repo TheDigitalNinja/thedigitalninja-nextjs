14dc4f12f7a90e252b42fecbf22313846936bdb9
Russell Perkins <dsw.ninjaboy@gmail.com>
2026-03-07 15:06:53 -0700

    (#49) Install Vercel Speed Insights and Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Inter } from "next/font/google";
 import Script from 'next/script';
 import { AuthProvider } from '../components/AuthProvider';
@@ -38,6 +40,8 @@ export default function RootLayout({
         <AuthProvider>
           {children}
         </AuthProvider>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
